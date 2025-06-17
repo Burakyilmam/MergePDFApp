1f60ad3b2565e717336975d695512a3280e2a010
bls-gelistirici-1 <bls.yazilim.2@hotmail.com>
2025-06-17 13:38:27 +0300

    Add project files.

---
+++ MergePDFApp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace MergePDFApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.grdPDF = new DevExpress.XtraGrid.GridControl();
+            this.grdvPDF = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.pnlButon = new DevExpress.XtraEditors.PanelControl();
+            this.btnUploadPDF = new DevExpress.XtraEditors.SimpleButton();
+            this.btnMergePDF = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDownloadPDF = new DevExpress.XtraEditors.SimpleButton();
+            this.pdfViewer = new DevExpress.XtraPdfViewer.PdfViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPDF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdvPDF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlButon)).BeginInit();
+            this.pnlButon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grdPDF
+            // 
+            this.grdPDF.Dock = System.Windows.Forms.DockStyle.Left;
+            this.grdPDF.Location = new System.Drawing.Point(0, 0);
+            this.grdPDF.MainView = this.grdvPDF;
+            this.grdPDF.Name = "grdPDF";
+            this.grdPDF.Size = new System.Drawing.Size(657, 391);
+            this.grdPDF.TabIndex = 7;
+            this.grdPDF.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.grdvPDF});
+            // 
+            // grdvPDF
+            // 
+            this.grdvPDF.GridControl = this.grdPDF;
+            this.grdvPDF.Name = "grdvPDF";
+            this.grdvPDF.OptionsBehavior.Editable = false;
+            this.grdvPDF.OptionsBehavior.ReadOnly = true;
+            this.grdvPDF.OptionsDetail.EnableMasterViewMode = false;
+            this.grdvPDF.OptionsSelection.MultiSelect = true;
+            this.grdvPDF.OptionsView.ShowGroupPanel = false;
+            // 
+            // pnlButon
+            // 
+            this.pnlButon.Controls.Add(this.btnUploadPDF);
+            this.pnlButon.Controls.Add(this.btnMergePDF);
+            this.pnlButon.Controls.Add(this.btnDownloadPDF);
+            this.pnlButon.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlButon.Location = new System.Drawing.Point(0, 391);
+            this.pnlButon.Name = "pnlButon";
+            this.pnlButon.Size = new System.Drawing.Size(1277, 35);
+            this.pnlButon.TabIndex = 8;
+            // 
+            // btnUploadPDF
+            // 
+            this.btnUploadPDF.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnUploadPDF.ImageOptions.SvgImage")));
+            this.btnUploadPDF.ImageOptions.SvgImageSize = new System.Drawing.Size(24, 24);
+            this.btnUploadPDF.Location = new System.Drawing.Point(488, 6);
+            this.btnUploadPDF.Name = "btnUploadPDF";
+            this.btnUploadPDF.Size = new System.Drawing.Size(119, 23);
+            this.btnUploadPDF.TabIndex = 0;
+            this.btnUploadPDF.Text = "Upload PDF";
+            // 
+            // btnMergePDF
+            // 
+            this.btnMergePDF.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnMergePDF.ImageOptions.SvgImage")));
+            this.btnMergePDF.ImageOptions.SvgImageSize = new System.Drawing.Size(24, 24);
+            this.btnMergePDF.Location = new System.Drawing.Point(613, 6);
+            this.btnMergePDF.Name = "btnMergePDF";
+            this.btnMergePDF.Size = new System.Drawing.Size(112, 23);
+            this.btnMergePDF.TabIndex = 0;
+            this.btnMergePDF.Text = "Merge PDF";
+            // 
+            // btnDownloadPDF
+            // 
+            this.btnDownloadPDF.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnDownloadPDF.ImageOptions.SvgImage")));
+            this.btnDownloadPDF.ImageOptions.SvgImageSize = new System.Drawing.Size(24, 24);
+            this.btnDownloadPDF.Location = new System.Drawing.Point(731, 6);
+            this.btnDownloadPDF.Name = "btnDownloadPDF";
+            this.btnDownloadPDF.Size = new System.Drawing.Size(159, 23);
+            this.btnDownloadPDF.TabIndex = 0;
+            this.btnDownloadPDF.Text = "Download PDF";
+            // 
+            // pdfViewer
+            // 
+            this.pdfViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfViewer.Location = new System.Drawing.Point(657, 0);
+            this.pdfViewer.Name = "pdfViewer";
+            this.pdfViewer.Size = new System.Drawing.Size(620, 391);
+            this.pdfViewer.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1277, 426);
+            this.Controls.Add(this.pdfViewer);
+            this.Controls.Add(this.grdPDF);
+            this.Controls.Add(this.pnlButon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdPDF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdvPDF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlButon)).EndInit();
+            this.pnlButon.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraGrid.GridControl grdPDF;
+        private DevExpress.XtraGrid.Views.Grid.GridView grdvPDF;
+        private DevExpress.XtraEditors.PanelControl pnlButon;
+        private DevExpress.XtraEditors.SimpleButton btnUploadPDF;
+        private DevExpress.XtraEditors.SimpleButton btnMergePDF;
+        private DevExpress.XtraEditors.SimpleButton btnDownloadPDF;
+        private DevExpress.XtraPdfViewer.PdfViewer pdfViewer;
+    }
+}
+
---
+++ MergePDFApp/Form1.cs
@@ -0,0 +1,402 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraSplashScreen;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MergePDFApp
+{
+    public partial class Form1 : Form
+    {
+        public class PDFItem
+        {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
+            public int PageSize { get; set; }
+            public bool IsMerged { get; set; }
+            public List<string> MergedFiles { get; set; } = new List<string>();
+            public string MergedFromDisplay => IsMerged && MergedFiles.Any() ? string.Join(", ", MergedFiles.Select(x => Path.GetFileName(x))) : "";
+        }
+        public string outputPath;
+        public BindingList<PDFItem> pdfList;
+        public PDFItem selectedPDF;
+        public int[] selectedPDFs;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.SizeChanged += Form1_SizeChanged;
+            this.Load += Form1_Load;
+            grdvPDF.FocusedRowChanged += grdvPDF_FocusedRowChanged;
+            grdvPDF.DoubleClick += grdvPDF_DoubleClick;
+            grdvPDF.KeyDown += grdvPDF_KeyDown;
+            grdvPDF.PopupMenuShowing += GrdvPDF_PopupMenuShowing;
+            grdvPDF.RowStyle += grdvPDF_RowStyle;
+            grdvPDF.RowClick += grdvPDF_RowClick;
+
+            btnDownloadPDF.Click += btnDownloadMergedPDF_Click;
+            btnMergePDF.Click += btnMergePDF_Click;
+            btnUploadPDF.Click += btnUploadPDF_Click;
+
+            GridControlDataSource();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ButtonCenter();
+        }
+
+        private void grdvPDF_RowClick(object sender, DevExpress.XtraGrid.Views.Grid.RowClickEventArgs e)
+        {
+            if (e.RowHandle >= 0)
+            {
+                PDFItem selectedItem = grdvPDF.GetRow(e.RowHandle) as PDFItem;
+                if (selectedItem != null && File.Exists(selectedItem.FilePath))
+                {
+                    pdfViewer.LoadDocument(selectedItem.FilePath);
+                }
+            }
+        }
+
+        private void grdvPDF_RowStyle(object sender, DevExpress.XtraGrid.Views.Grid.RowStyleEventArgs e)
+        {
+            if (e.RowHandle >= 0)
+            {
+                PDFItem item = grdvPDF.GetRow(e.RowHandle) as PDFItem;
+                if (item != null)
+                {
+                    if (item.IsMerged)
+                    {
+                        e.Appearance.ForeColor = Color.Red;
+                    }
+                }
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            ButtonCenter();
+        }
+
+        public void GridAppearance()
+        {
+            Font font = new Font(grdvPDF.Appearance.HeaderPanel.Font, FontStyle.Bold);
+
+            grdvPDF.Columns[0].Caption = "Dosya Adı";
+            grdvPDF.Columns[0].AppearanceHeader.Font = font;
+            grdvPDF.Columns[0].AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+
+            grdvPDF.Columns[1].Caption = "Dosya Yolu";
+            grdvPDF.Columns[1].AppearanceHeader.Font = font;
+            grdvPDF.Columns[1].AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+
+            grdvPDF.Columns[2].Caption = "Sayfa Sayısı";
+            grdvPDF.Columns[2].AppearanceHeader.Font = font;
+            grdvPDF.Columns[2].AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+
+            grdvPDF.Columns[3].Visible = false;
+
+            grdvPDF.Columns[4].Caption = "Birleştirildiği Dosyalar";
+            grdvPDF.Columns[4].AppearanceHeader.Font = font;
+            grdvPDF.Columns[4].AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+        }
+
+        public void GridControlDataSource()
+        {
+            pdfList = new BindingList<PDFItem>();
+            grdPDF.DataSource = pdfList;
+            GridAppearance();
+        }
+
+        public void ButtonCenter()
+        {
+            int totalButtonsWidth = 0;
+            int spacing = 10;
+
+            foreach (Control control in pnlButon.Controls)
+            {
+                if (control is SimpleButton) totalButtonsWidth += control.Width + spacing;
+            }
+
+            totalButtonsWidth -= spacing;
+
+            int startX = (pnlButon.Width - totalButtonsWidth) / 2;
+            int currentX = startX;
+
+            foreach (Control control in pnlButon.Controls)
+            {
+                if (control is SimpleButton)
+                {
+                    control.Location = new Point(currentX, (pnlButon.Height - control.Height) / 2);
+                    currentX += control.Width + spacing;
+                }
+            }
+        }
+
+        private void grdvPDF_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            if (e.FocusedRowHandle >= 0)
+            {
+                selectedPDF = grdvPDF.GetFocusedRow() as PDFItem;
+            }
+        }
+
+        public void OpenPDF(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+                {
+                    FileName = filePath,
+                    UseShellExecute = true
+                });
+            }
+            else
+            {
+                MessageBox.Show("PDF dosyası bulunamadı.");
+            }
+        }
+
+        public void DeletePDF()
+        {
+            selectedPDFs = grdvPDF.GetSelectedRows();
+
+            if (selectedPDFs == null || selectedPDFs.Length == 0) return;
+
+            foreach (var rowHandle in selectedPDFs.OrderByDescending(x => x))
+            {
+                PDFItem deleted = grdvPDF.GetRow(rowHandle) as PDFItem;
+                if (deleted != null) pdfList.Remove(deleted);
+            }
+
+            MessageBox.Show("Seçili PDF'ler başarıyla silindi.");
+        }
+
+        private void grdvPDF_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                OpenPDF(selectedPDF.FilePath);
+            }
+
+            if (e.KeyCode == Keys.Delete)
+            {
+                DeletePDF();
+            }
+        }
+
+        private void grdvPDF_DoubleClick(object sender, EventArgs e)
+        {
+            if (selectedPDF != null)
+            {
+                OpenPDF(selectedPDF.FilePath);
+            }
+        }
+
+        public void UploadPDF()
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "PDF files (*.pdf)|*.pdf",
+                Multiselect = true
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var file in ofd.FileNames)
+                {
+                    PdfDocument doc = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+                    pdfList.Add(new PDFItem
+                    {
+                        FileName = Path.GetFileName(file),
+                        FilePath = file,
+                        PageSize = doc.PageCount,
+                        IsMerged = false
+                    });
+                }
+            }
+        }
+
+
+        private void btnUploadPDF_Click(object sender, EventArgs e)
+        {
+            UploadPDF();
+        }
+
+        public void MergeSelectedPDF()
+        {
+            selectedPDFs = grdvPDF.GetSelectedRows();
+            if (selectedPDFs == null || selectedPDFs.Length < 2)
+            {
+                MessageBox.Show("Birleştirmek için en az iki PDF seçmelisiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            outputPath = Path.Combine(Path.GetTempPath(), DateTime.Now.ToString("yyMMddHHmmss") + "_Merged.pdf");
+            PdfDocument outputDoc = new PdfDocument();
+
+            var selectedItems = selectedPDFs.Select(x => grdvPDF.GetRow(x) as PDFItem).Where(x => x != null).ToList();
+
+            foreach (var item in selectedItems)
+            {
+                PdfDocument inputDoc = PdfReader.Open(item.FilePath, PdfDocumentOpenMode.Import);
+                foreach (PdfPage page in inputDoc.Pages)
+                {
+                    outputDoc.AddPage(page);
+                }
+            }
+
+            outputDoc.Save(outputPath);
+
+            if (!pdfList.Any(p => string.Equals(p.FilePath, outputPath, StringComparison.OrdinalIgnoreCase)))
+            {
+                var mergedDoc = PdfReader.Open(outputPath, PdfDocumentOpenMode.Import);
+                pdfList.Add(new PDFItem
+                {
+                    FileName = Path.GetFileName(outputPath),
+                    FilePath = outputPath,
+                    PageSize = mergedDoc.PageCount,
+                    IsMerged = true,
+                    MergedFiles = selectedItems.Select(p => p.FilePath).ToList()
+                });
+            }
+
+            MessageBox.Show("Seçili PDF'ler başarıyla birleştirildi.");
+        }
+
+        public void MergeAllPDF()
+        {
+            outputPath = Path.Combine(Path.GetTempPath(), "Merged.pdf");
+            PdfDocument outputDoc = new PdfDocument();
+
+            foreach (var pdf in pdfList)
+            {
+                PdfDocument inputDoc = PdfReader.Open(pdf.FilePath, PdfDocumentOpenMode.Import);
+                foreach (PdfPage page in inputDoc.Pages)
+                {
+                    outputDoc.AddPage(page);
+                }
+            }
+
+            outputDoc.Save(outputPath);
+
+            if (!pdfList.Any(p => string.Equals(p.FilePath, outputPath, StringComparison.OrdinalIgnoreCase)))
+            {
+                var mergedDoc = PdfReader.Open(outputPath, PdfDocumentOpenMode.Import);
+                pdfList.Add(new PDFItem
+                {
+                    FileName = Path.GetFileName(outputPath),
+                    FilePath = outputPath,
+                    PageSize = mergedDoc.PageCount,
+                    IsMerged = true,
+                    MergedFiles = pdfList.Select(p => p.FilePath).ToList()
+                }); ;
+            }
+
+            MessageBox.Show("Tüm PDF'ler başarıyla birleştirildi.");
+        }
+
+        private void btnMergePDF_Click(object sender, EventArgs e)
+        {
+            MergeSelectedPDF();
+        }
+
+        private string GetUniqueFileName(string filePath)
+        {
+            string directory = Path.GetDirectoryName(filePath);
+            string filenameWithoutExt = Path.GetFileNameWithoutExtension(filePath);
+            string extension = Path.GetExtension(filePath);
+
+            string uniquePath = filePath;
+            int counter = 1;
+
+            while (File.Exists(uniquePath))
+            {
+                uniquePath = Path.Combine(directory, $"{filenameWithoutExt}_{counter}{extension}");
+                counter++;
+            }
+
+            return uniquePath;
+        }
+
+        public void DownloadPDF()
+        {
+            selectedPDFs = grdvPDF.GetSelectedRows();
+
+            if (selectedPDFs == null || selectedPDFs.Length == 0)
+            {
+                MessageBox.Show("Lütfen en az bir PDF seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var selectedItems = selectedPDFs
+                .Select(x => grdvPDF.GetRow(x) as PDFItem)
+                .Where(x => x != null)
+                .Reverse()
+                .ToList();
+
+            if (selectedItems.Count == 1)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "PDF files (*.pdf)|*.pdf";
+                sfd.FileName = selectedItems[0].FileName;
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.Copy(selectedItems[0].FilePath, sfd.FileName, true);
+                        MessageBox.Show("PDF başarıyla kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                fbd.Description = "PDF'lerin kaydedileceği klasörü seçiniz.";
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var item in selectedItems)
+                    {
+                        try
+                        {
+                            string destinationPath = Path.Combine(fbd.SelectedPath, item.FileName);
+
+                            destinationPath = GetUniqueFileName(destinationPath);
+
+                            File.Copy(item.FilePath, destinationPath, true);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"'{item.FileName}' dosyası kopyalanırken hata oluştu:\n{ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+
+                    MessageBox.Show("Seçili PDF'ler başarıyla kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void btnDownloadMergedPDF_Click(object sender, EventArgs e)
+        {
+            DownloadPDF();
+        }
+
+        private void GrdvPDF_PopupMenuShowing(object sender, DevExpress.XtraGrid.Views.Grid.PopupMenuShowingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
